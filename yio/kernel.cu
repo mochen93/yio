d55bc2e5c0c3c9cc384b19f65ecf22da508f079e
晨 杨 <2310384192@qq.com>
2020-07-03 11:12:38 +0800

    建立分支branch1并修改

--- yio/kernel.cu
+++ yio/kernel.cu
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 
+#define check 1; 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
 __global__ void addKernel(int *c, const int *a, const int *b)
